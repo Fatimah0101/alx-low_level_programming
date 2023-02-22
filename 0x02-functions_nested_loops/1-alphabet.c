ef9d481762c91b8217fe75099e2d97712fa33ae8
Fatimah0101 <phazimore911@gmail.com>
2023-02-22 08:33:19 -0800

    1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	char alp;
+	int i;
+
+	for (i = 1; i <= 10;)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+		i++;
+		_putchar('\n');
+	}
+}
